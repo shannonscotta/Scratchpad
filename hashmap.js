926cc83b6001850c00e9134ee19ccf89a26dd999
Scott Shannon <78231948+shannonscotta@users.noreply.github.com>
2023-11-09 18:19:08 -0800

    refactor algo after breaking down each line

--- hashmap.js
+++ hashmap.js
@@ -1,42 +1,18 @@
-
-let nums1 = [8,2,1,5,4,9,0];
-let target1 = [2];
-
+// let nums1 = [7,3,1,8,2,4,10];
+// let target1 = [11];
 
 function twoSum(nums, target) {
+  let hashMap = new Map();
 
-    let hashMap = new Map();
-    let iterations = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let neededNumber = target - nums[i];
 
-    for(let i = 0; i < nums.length; i++) {
-
-        iterations++;
-
-        let neededNumber = target - nums[i];
-        // 2 - 8 = -6
-        // neededNum = -6
-
-        console.log(`neededNumber: ${target - nums[i]} = target: ${target} - nums[i]: ${nums[i]}`);
-
-        //does our hashMap have -6?
-        if(hashMap.has(neededNumber)) {
-
-            console.log(`inside of if \n hashMap.has(${neededNumber})`);
-
-            // if it does, we will return i and neededNum
-            console.log(`hashMap.get(neededNumber) ***********`, hashMap.get(neededNumber))
-            console.log(`Total iterations: ${iterations}`);
-            return [i, hashMap.get(neededNumber)];
-        } 
-        // regardless of if our hashmap has the num, we will add it to our hashmap, 
-        // hashmap.set(value, index)
-        hashMap.set(nums[i], i);
-        console.log(`SETTING HASHMAP ----> nums[i]:${nums[i]}, i: ${i}`);
-        // set(8, 0)
-        console.log('\n')
+    if (hashMap.has(neededNumber)) {
+      return [i, hashMap.get(neededNumber)];
     }
-};
 
+    hashMap.set(nums[i], i);
+  }
+}
 
-console.log(`Expected: [1, 6], got:`, twoSum(nums1, target1));
-
+// console.log(twoSum(nums1, target1));
