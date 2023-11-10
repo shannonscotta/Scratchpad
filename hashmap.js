5a0c2dceb7507651d069aabb878684f99a69344c
Scott Shannon <78231948+shannonscotta@users.noreply.github.com>
2023-11-09 17:39:46 -0800

    walkthrough of hashmaps

--- hashmap.js
+++ hashmap.js
@@ -4,16 +4,28 @@ let tar = [2];
 
 
 function twoSum(nums, target) {
+
     let hashMap = new Map();
 
     for(let i = 0; i < nums.length; i++) {
-        let neededNumber = target - nums[i];
 
+        let neededNumber = target - nums[i];
+        // 2 - 8 = -6
+        // neededNum = -6
+
+        console.log(`neededNumber: ${target - nums[i]} = target: ${target} - nums[i]: ${nums[i]}`);
+
+        //does our hashMap have -6?
         if(hashMap.has(neededNumber)) {
+
+            console.log(`inside of if \n hashMap.has(${neededNumber})`);
+
+            // if it does, we will return i and neededNum
             return [i, hashMap.get(neededNumber)];
         } 
+        // regardless of if our hashmap has the num, we will add it to our hashmap, 
+        // hashmap.set(value, index)
         hashMap.set(nums[i], i);
-
     }
 };
 
