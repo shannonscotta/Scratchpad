141996deca428249ddadbbe8acf6bd813483993e
Scott Shannon <78231948+shannonscotta@users.noreply.github.com>
2023-11-09 18:02:38 -0800

    picked apart hashmap

--- hashmap.js
+++ hashmap.js
@@ -1,13 +1,16 @@
 
-let arr = [8,2,1,5,4,9,0];
-let tar = [2];
+let nums1 = [8,2,1,5,4,9,0];
+let target1 = [2];
 
 
 function twoSum(nums, target) {
 
     let hashMap = new Map();
+    let iterations = 0;
 
     for(let i = 0; i < nums.length; i++) {
+
+        iterations++;
 
         let neededNumber = target - nums[i];
         // 2 - 8 = -6
@@ -21,14 +24,19 @@ function twoSum(nums, target) {
             console.log(`inside of if \n hashMap.has(${neededNumber})`);
 
             // if it does, we will return i and neededNum
+            console.log(`hashMap.get(neededNumber) ***********`, hashMap.get(neededNumber))
+            console.log(`Total iterations: ${iterations}`);
             return [i, hashMap.get(neededNumber)];
         } 
         // regardless of if our hashmap has the num, we will add it to our hashmap, 
         // hashmap.set(value, index)
         hashMap.set(nums[i], i);
+        console.log(`SETTING HASHMAP ----> nums[i]:${nums[i]}, i: ${i}`);
+        // set(8, 0)
+        console.log('\n')
     }
 };
 
 
-console.log(`Expected: [1, 6], got:`, twoSum(arr, tar));
+console.log(`Expected: [1, 6], got:`, twoSum(nums1, target1));
 
